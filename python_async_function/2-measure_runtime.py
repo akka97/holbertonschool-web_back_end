605a8ac114facea31616e244f64853f2d6c84cf9
akka97 <akapellari@yahoo.com>
2023-08-29 04:20:41 -0700

    task

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""documentation"""
+import random
+import asyncio
+
+
+wait_random = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """messure time wait_n"""
+    start_time = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    elapse_time = end_time - start_time
+    return elapse_time / n
