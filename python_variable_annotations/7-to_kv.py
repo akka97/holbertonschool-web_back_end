44a9f82a89a65d76545f40446b411cf0288a3ffa
akka97 <akapellari@yahoo.com>
2023-08-29 02:39:15 -0700

    tasks

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """documentation"""
 
 
-from typing import List, Union, Tuple
+from typing import Union, List, Tuple
 
 
-def to_ktv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """from element to tuple"""
     sec_element = v ** 2
     return (k, sec_element)
