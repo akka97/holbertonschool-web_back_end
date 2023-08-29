6e7dc58705c26da0a10fe4ebb4a6a53bce2659c1
akka97 <akapellari@yahoo.com>
2023-08-29 02:44:14 -0700

    task

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""documentation"""
+
+
+from typing import Callable, Iterator, Union, Optional
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], [float]:
+    """make callable"""
+    def make_multiply(x: float) -> float:
+        return (x * multiplier)
+    return make_multiply
+
