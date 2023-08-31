d6153c8d825d2d54e0ce7e838924d6ba1874236f
akka97 <akapellari@yahoo.com>
2023-08-31 14:09:41 +0200

    tasks

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''async file'''
+import asyncio
+import random
+from typing import Generator, List
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    '''async generator'''
+    [element for element in async_generator()]
