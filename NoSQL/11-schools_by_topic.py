81a1035721f9974196b4f6e4a27392bd51ee5647
akka97 <akapellari@yahoo.com>
2023-09-07 02:45:54 -0700

    search topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Return the list of school having a specific topic"""
-    result = mongo_collection.find({'topics': {'$in': [topic]}}})
+    result = mongo_collection.find({'topics': {'$in': [topic]}})
     return result
