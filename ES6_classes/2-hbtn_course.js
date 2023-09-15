7468a4d2f30466fb654762dc891b5f7371f60d91
akka97 <akapellari@yahoo.com>
2023-09-15 13:44:49 +0200

    task

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -9,33 +9,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new Error('Name must be a string');
     }
     this._name = name;
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new Error('Length must be a number');
     }
     this._length = length;
   }
-  
+
   get students() {
     return this._students;
   }
-  
+
   set students(students) {
     if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
       throw new Error('Students must be an array of strings');
