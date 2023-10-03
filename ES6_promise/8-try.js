d18615adbe6dd6d576550bdcb2ad479c59344e28
akka97 <akapellari@yahoo.com>
2023-10-03 15:14:09 +0200

    tasks

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,7 @@
-export default function divideFunction(numerator, deminator) {
-    if (deminator === 0) {
-        throw new Error('cannot devide by 0');
-    } else {
-        return numerator / deminator;
-    }
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot devide by 0');
+  } else {
+    return numerator / deminator;
+  }
 }
