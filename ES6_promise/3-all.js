de4c2498b375ba1e774450cdaa4a13b24c94c068
akka97 <akapellari@yahoo.com>
2023-10-03 13:45:13 +0200

    tasks

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((Response) => {
-        console.log(`${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`);
+      console.log(`${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
