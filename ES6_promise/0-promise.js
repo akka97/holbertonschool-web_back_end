af082cfea302df5c6db8e235f22858f54465e95c
akka97 <akapellari@yahoo.com>
2023-10-03 11:55:12 +0200

    task

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 function getResponseFromAPI() {
   return new Promise((resolve) => {
-  setTimeout(() => {
-        resolve();
+    setTimeout(() => {
+      resolve();
     }, 10);
-});
+  });
 }
 
 export default getResponseFromAPI;
