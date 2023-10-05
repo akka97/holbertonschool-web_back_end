09a311d4235b2fd4e81c3f872093310d03f3cc0b
akka97 <akapellari@yahoo.com>
2023-10-05 12:57:36 +0200

    third task

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getListStudentsByLocation(listOfStudent, city) {
-    return listOfStudent.filter((cities) => cities.location === city);
+  return listOfStudent.filter((cities) => cities.location === city);
 }
