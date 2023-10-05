2553a499f2c87f14594646854e32e1a9bdac21f6
akka97 <akapellari@yahoo.com>
2023-10-05 13:17:27 +0200

    add

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(listOfStudents, city, newGrades
     const students = listOfStudents.filter((student) => student.location === city);
 
     const updatedStudent = students.map((student) => {
-        const grades = newGrades?.find((grade) => grade.studentId === student.id);
+        const grades = newGrades?find((grade) => grade.studentId === student.id);
         //? -> throws error if array undefined
 
         const grade = grades ? grades.grade : 'N/A';
