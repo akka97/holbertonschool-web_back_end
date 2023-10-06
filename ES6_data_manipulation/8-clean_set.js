c83391117402b2e8f418250518157b3afc099b5a
akka97 <akapellari@yahoo.com>
2023-10-06 11:55:50 +0200

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,22 @@
 export default function cleanSet(set, startString) {
-  const arr = [];
-  if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
-    return '';
+  // Check if set is a Set
+  if (!(set instanceof Set)) {
+    throw new Error('The first argument must be a Set.');
   }
+
+  // Check if startString is a non-empty string
+  if (typeof startString !== 'string' || startString === '') {
+    throw new Error('The second argument must be a non-empty string.');
+  }
+
+  const arr = [];
+
   set.forEach((element) => {
-    if (typeof element !== 'undefined') {
-      if (element.startsWith(startString)) {
-        arr.push(element.split(startString)[1]);
-      }
+    // Check if element is defined and starts with startString
+    if (element !== undefined && element.startsWith(startString)) {
+      arr.push(element.substring(startString.length));
     }
   });
+
   return arr.join('-');
 }
